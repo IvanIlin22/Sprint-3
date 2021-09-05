d4e7e73e39788ae5b123d1eda658ec6c945f1a0f
Ivan Ilyin <geniusmov@yandex.ru>
2021-09-05 16:40:47 +0300

    add ioHomework

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,5 +1,11 @@
 package ru.sber.io
 
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
@@ -11,7 +17,26 @@ class Archivator {
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
+        val fileInput: String  = "io\\logfile.log";
+        val fileOutput: String = "io\\logfile.zip";
+        try {
+            val zos = ZipOutputStream(FileOutputStream(fileOutput));
+            val fis = FileInputStream(fileInput);
+            val entry = ZipEntry("logfile.log");
+            val buffer = ByteArray(fis.available());
 
+            fis.use {
+                fis.read(buffer);
+            }
+
+            zos.use {
+                zos.putNextEntry(entry);
+                zos.write(buffer);
+                zos.closeEntry();
+            }
+        } catch(ex: Exception) {
+            println(ex.message);
+        }
     }
 
     /**
@@ -20,5 +45,31 @@ class Archivator {
      */
     fun unzipLogfile() {
 
+        val fileInput = "io\\logfile.zip";
+        val fileOutput = "io\\unzippedLogfile.log";
+        try {
+            val zis = ZipInputStream(FileInputStream(fileInput));
+
+            zis.use {
+                while (zis.nextEntry != null) {
+
+                    val fos = FileOutputStream(fileOutput);
+                    fos.use {
+                        fos.write(zis.readAllBytes());
+                    }
+
+                    zis.closeEntry();
+                }
+            }
+
+        } catch (ex: Exception) {
+            println(ex.message);
+        }
     }
+}
+
+fun main() {
+    val arch: Archivator = Archivator();
+    arch.zipLogfile();
+    arch.unzipLogfile();
 }
