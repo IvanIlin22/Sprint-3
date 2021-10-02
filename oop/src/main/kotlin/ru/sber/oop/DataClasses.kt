1ee4272ffa51bc44834c13fe5d099b3d82321312
Ivan <58389800+IvanIlin22@users.noreply.github.com>
2021-10-02 16:51:46 +0300

    add oopHomework (#2)
    * add oopHomework

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -1,11 +1,25 @@
 package ru.sber.oop
 
+import java.util.*
+
 data class User(val name: String, val age: Long) {
     lateinit var city: String
+
+    override fun hashCode(): Int {
+        return Objects.hash(name, age, city);
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null || javaClass != other.javaClass) return false;
+        val user = other as User;
+        return user.name == name && user.age == age && user.city == city;
+    }
 }
 
 fun main() {
     val user1 = User("Alex", 13)
-    //TODO: user2 = ...
-    //TODO: user3 = ...
+    val user2 = user1.copy(name = "Kate");
+    user1.city = "Omsk";
+    val user3 = user1.copy();
+    user3.city = "Tomsk";
 }
--- oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
+++ oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
@@ -1,13 +1,58 @@
 package ru.sber.oop
 
+import kotlin.random.Random
+
 interface Fightable {
     val powerType: String
     var healthPoints: Int
     val damageRoll: Int
+        get() = Random.nextInt(0, 100);
 
     fun attack(opponent: Fightable): Int
 }
 
-//TODO: create class Player, Monster, Goblin here...
+class Player(override val powerType: String,
+             override var healthPoints: Int,
+             val name: String,
+             val isBlessed: Boolean) : Fightable {
 
+    override val damageRoll: Int = super.damageRoll;
 
+    override fun attack(opponent: Fightable): Int {
+        if (isBlessed) {
+            opponent.healthPoints -= damageRoll * 2;
+            return damageRoll * 2;
+        }
+        opponent.healthPoints -= damageRoll;
+
+        return damageRoll;
+    }
+}
+
+abstract class Monster(override val powerType: String,
+                       override var healthPoints: Int,
+                       val name: String,
+                       val description: String) : Fightable {
+
+    override val damageRoll: Int = super.damageRoll;
+
+    override fun attack(opponent: Fightable): Int {
+        opponent.healthPoints -= damageRoll;
+
+        return damageRoll;
+    }
+
+    fun getSalutation(): String {
+
+        return "Hello I'm monster";
+    }
+}
+
+class Goblin(powerType: String,
+             healthPoints: Int,
+             name: String,
+             description: String) : Monster(powerType, healthPoints, name, description) {
+
+    override val damageRoll: Int
+        get() = super.damageRoll / 2;
+}
